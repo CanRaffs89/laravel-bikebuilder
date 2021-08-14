f3081f1bb2127b11dbc56070f422fa2a7e650dde
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-08-14 21:57:47 +0100

    Styled welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,12 @@
-@extends('layouts.layout')
+@extends('layouts.app')
 @section('content')
-    <div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0 flex-column">
-                <img src="/img/logo.png" alt="" class="img-logo">
-                <h1 class="text-center">Bike Builder</h1>
-                <a class="text-center" href="{{ route('orders.create') }}">Create Order</a>
-                <a class="text-center" href="{{ route('orders.index') }}">View Orders</a>
-                <p class="message">{{ session('success-msg') }}</p>
-            </div> 
-        </div>
+    <div class="outer-container flex flex-column justify-center">
+        <div class="flex justify-evenly flex-column inner-container">
+            <img src="/img/logo.png" alt="" class="img-logo">
+            <button class="btn btn-info btn-welcome"><a class="btn-welcome-text" href="{{ route('orders.create') }}">Create Order</a></button>
+            <button class="btn btn-info btn-welcome"><a class="btn-welcome-text" href="{{ route('orders.index') }}">Staff Login</a></button>
+            <p class="message">{{ session('success-msg') }}</p>
+        </div> 
     </div>
 @endsection
 
