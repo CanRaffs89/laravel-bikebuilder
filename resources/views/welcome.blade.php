a238194360c4e0371754a1509eece6794dec3533
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-08-15 11:23:28 +0100

    Consolidated CSS, added new logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     <div class="outer-container flex flex-column justify-center">
         <div class="flex justify-evenly flex-column inner-container">
-            <img src="/img/logo.png" alt="" class="img-logo">
+            <img src="/img/roundel_logo.png" alt="" class="img-logo">
             <button class="btn btn-info btn-welcome"><a class="btn-welcome-text" href="{{ route('orders.create') }}">Create Order</a></button>
-            <button class="btn btn-info btn-welcome"><a class="btn-welcome-text" href="{{ route('orders.index') }}">Staff Login</a></button>
+            <button class="btn btn-info btn-welcome"><a class="btn-welcome-text" href="{{ route('orders.index') }}">View Orders</a></button>
             <p class="message">{{ session('success-msg') }}</p>
         </div> 
     </div>
